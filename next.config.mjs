3876c45eb2ab358988b0a23c704d8bb5659f80a1
Mehadi Hossain <mehadi_hossain@yahoo.com>
2025-12-11 10:02:19 +0600

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Enable static export
+  reactStrictMode: true,
+  basePath: "/portfolio-2025",
   images: {
     unoptimized: true, // Disable Image Optimization API as it's not available for static exports
   },
   // If you're using a basePath (for GitHub Pages user/organization sites)
-  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/portfolio-2025' : '',
   // If you're using assetPrefix (for GitHub Pages project sites)
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio-2025/' : '',
 }
 
 export default nextConfig
